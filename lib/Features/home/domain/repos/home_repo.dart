e887a35a2de92bf484f48ecc8117984384e8a01c
HW <khaleddortmound123@gmail.com>
2023-11-28 01:27:07 +0200

    Initial commit

---
+++ lib/Features/home/domain/repos/home_repo.dart
@@ -0,0 +1,19 @@
+
+
+import 'package:bookly/Features/home/domain/entities/book_entity.dart';
+import 'package:dartz/dartz.dart';
+
+import '../../../../core/errors/failure.dart';
+
+abstract class HomeRepo
+{
+
+  Future<Either<Failure,List<BookEntity>>> fetchFeaturedBooks();
+
+  Future<Either<Failure,List<BookEntity>>> fetchBestSellerBooks();
+
+
+
+
+
+}
